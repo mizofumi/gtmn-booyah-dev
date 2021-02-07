8067436271fb62910d2688ce27cabfa001d55dbb
k3nNyJP <k3nny.51rcy@gmail.com>
2021-02-07 17:24:32 +0900

    change server script for render vue files

--- src/app.py
+++ src/app.py
@@ -1,8 +1,12 @@
 from flask import Flask, Response, request, jsonify, render_template
 
-app = Flask(__name__)
- 
+app = Flask(__name__, static_folder='./frontend/dist/static',
+            template_folder='./frontend/dist')
+
 # トップページ
-@app.route("/")
-def index():
+
+
+@app.route("/", defaults={'path': ''})
+@app.route("/<path:path>")
+def index(path):
     return render_template('index.html')
