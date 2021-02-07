e8bbf654fb6dd7e6d162ba3ae84086e389a89ffe
k3nNyJP <k3nny.51rcy@gmail.com>
2021-02-07 17:40:07 +0900

    rewrite pages by vue

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,117 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <header class="header">
+    <div class="header-image-container">
+      <h1 class="header-title">Welcome to げーたみん Minecraft</h1>
+
+      <img class="header-image" src="http://placehold.jp/400x400.png" />
+    </div>
+
+    <p class="server-description">
+      こちらはjava版、PS4以外の統合版Minecraftで利用できます。<br />
+      みんなでワイワイするもより！時には1人で黙々やるのもよし！それぞれの楽しみ方でマナーを守りつつ素敵なマイクラライフを送りましょう！
+    </p>
+
+    <div class="status">
+      <div class="server-status">
+        <h3>現在サーバー</h3>
+        <span>稼働中</span>
+      </div>
+      <div class="user-status">
+        <h3 class="user-status-header">オンラインユーザー</h3>
+        <div>john doe</div>
+        <div>名無しの権兵衛</div>
+      </div>
+    </div>
+  </header>
+  <article></article>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 export default defineComponent({
   name: "Home",
-  components: {
-    HelloWorld
-  }
 });
 </script>
+
+<style lang="scss" scoped>
+.header {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #eeeeee;
+}
+
+.header-image-container {
+  position: relative;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 32px;
+}
+
+.header-title {
+  position: absolute;
+  bottom: 64px;
+  text-align: center;
+}
+
+.header-image {
+  width: 400px;
+  height: 400px;
+}
+
+.server-description {
+  margin: 16px 32px 0;
+  max-width: 630px;
+}
+
+.status {
+  display: flex;
+  margin: 32px;
+}
+
+.status h3 {
+  margin: 8px 0;
+  font-size: 14px;
+  font-weight: 400;
+}
+
+.server-status {
+  text-align: center;
+  padding: 8px 48px 16px;
+  margin-right: 32px;
+  background-color: white;
+  border-radius: 8px;
+}
+
+.user-status {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.user-status > div {
+  position: relative;
+  padding: 8px 16px 8px 48px;
+  background-color: white;
+  border-radius: 100px;
+  margin-right: 8px;
+}
+
+.user-status > div::before {
+  position: absolute;
+  content: "";
+  width: 30px;
+  height: 30px;
+  background-color: #bbbbbb;
+  top: 4px;
+  left: 4px;
+  border-radius: 100px;
+}
+
+.user-status-header {
+  width: 100%;
+  flex: 0 0 auto;
+}
+</style>
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   assetsDir: "static",
+  pages: {
+    index: {
+      entry: "src/main.ts",
+      title: "GTMNマインクラフト",
+    },
+  },
 };
